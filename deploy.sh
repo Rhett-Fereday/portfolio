60e1af098e95e2c53067a535ce0a20c14bf0b1b6
Rhett <gmfereday@gmail.com>
2025-04-10 10:37:01 -0500

    updated deploy script, updated resume, removed unused images

--- deploy.sh
+++ deploy.sh
@@ -14,12 +14,12 @@ cd dist
 
 git init
 git add -A
-git commit -m 'deploy changes'
+git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Rhett-Fereday/portfolio.git main:gh-pages
+git push -f git@github.com:Rhett-Fereday/portfolio.git master:gh-pages
 
 cd -
